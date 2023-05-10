1e58ba92c4e2fd322cadb9d69a3683766ce17127
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-10 20:11:02 +0600

    Add, Delete and Update in MongoDB Implemented

--- index.js
+++ index.js
@@ -30,23 +30,47 @@ async function run() {
 
     const userCollection = client.db("usersDB").collection("users");
 
-    app.get('/users', async(req, res) =>{
+    app.get('/users', async (req, res) => {
       const cursor = userCollection.find()
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        console.log("New User", user);
-        const result = await userCollection.insertOne(user);
-        res.send(result);
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const user = await userCollection.findOne(query);
+      res.send(user);
     })
 
-    app.delete('/users/:id', async(req, res) =>{
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log("New User", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      console.log(user);
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          email: user.email
+        }
+      }
+
+      const result =  await userCollection.updateOne(filter, updatedUser, options)
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
       console.log('Please Delete From Database', id);
-      const query = { _id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result)
     })
@@ -64,10 +88,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('SIMPLE CRUD IS RUNNING')
+app.get('/', (req, res) => {
+  res.send('SIMPLE CRUD IS RUNNING')
 })
 
-app.listen(port, () =>{
-    console.log(`SIMPLE CRUD is running on PORT: ${port}`);
+app.listen(port, () => {
+  console.log(`SIMPLE CRUD is running on PORT: ${port}`);
 })
